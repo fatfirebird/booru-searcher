2c3d238734590dfa145f4aea61e1a28e372652d0
fatfirebird <fatfirebird@gmail.com>
2020-04-06 18:06:40 +0500

    testing another api

--- src/controllers/imagesController.ts
+++ src/controllers/imagesController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { search } from 'kaori';
 import { Image } from 'kaori/typings/Image';
+import * as Booru from 'booru';
 
 type QueryInterface = {
   booru: string;
@@ -11,28 +12,34 @@ type QueryInterface = {
 
 const render = async(req: Request, res: Response) => {
   try {
-    console.log(req.query)
+    // console.log(req.query)
     const { booru, random, exclude }: QueryInterface = req.query;
     const tags: Array<string> = req.query.tags.split(', ');
 
-    const images = await search(booru, { 
-      random, 
-      exclude,
-      limit: 20,
-      tags,
-    })
-    .catch(err => {
-      throw err
+    // const images = await search(booru, { 
+    //   random, 
+    //   exclude,
+    //   limit: 20,
+    //   tags,
+    // })
+    // .catch(err => {
+    //   throw err
+    // })
+
+    // const imagesUrl = images.map(image => {
+    //   return { 
+    //     preview: image.previewURL || image.fileURL, 
+    //     url: image.fileURL,
+    //     id: image.id
+    //   }
+    // });
+    // console.log(imagesUrl)
+
+    const images = await Booru.search(booru, tags, {limit: 20, random});
+    const imagesUrl = images.map(post => {
+      return {url: post.fileUrl, preview: post.postView, id: post.id}
     })
 
-    const imagesUrl = images.map(image => {
-      return { 
-        preview: image.previewURL || image.fileURL, 
-        url: image.fileURL,
-        id: image.id
-      }
-    });
-    console.log(imagesUrl)
     res.render('images', { imagesUrl });
 
   } catch (error) {
