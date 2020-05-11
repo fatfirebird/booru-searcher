1dd4e910e23ed34b31e48d9e355ddfa3f59c940a
fatfirebird <fatfirebird@gmail.com>
2020-05-11 22:22:08 +0500

    fix builded files issue

--- server/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var path = require("path");
-var render = function (req, res) {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-};
-exports["default"] = {
-    render: render
-};
--- server/routes/homeRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var homeController_1 = require("../controllers/homeController");
-var router = express.Router();
-router.get('/', homeController_1["default"].render);
-exports["default"] = router;
